1652e3bb93d3e1abdd2ac2d7cd33c54612c54891
mltaskova <mltaskova@n227-133.mtholyoke.edu>
2019-02-11 22:01:37 -0500

    added head and tail

--- src/main/java/edu/smith/cs/csc262/coopsh/apps/Head.java
+++ src/main/java/edu/smith/cs/csc262/coopsh/apps/Head.java
@@ -1,5 +1,6 @@
 package edu.smith.cs.csc262.coopsh.apps;
 
+import edu.smith.cs.csc262.coopsh.InputLine;
 import edu.smith.cs.csc262.coopsh.ShellEnvironment;
 import edu.smith.cs.csc262.coopsh.Task;
 
@@ -7,12 +8,32 @@ import edu.smith.cs.csc262.coopsh.Task;
  * Created by mltaskova on 2/10/19.
  */
 public class Head extends Task {
+
+    int numElt;
+    StringBuilder sb = new StringBuilder();
+
     public Head(ShellEnvironment env, String[] args) {
         super(env, args);
+        this.numElt = Integer.parseInt(args[0]);
     }
 
     @Override
     protected void update() {
+        InputLine line = this.input.poll();
+        if (line == null) {
+            // still waiting for more...
+            return;
+        }
 
+        // only output and print when we've seen the whole file!
+        if (numElt == 0) {
+            this.closeOutput();
+            this.exit(0);
+            return;
+        }
+
+        // Otherwise, increment this count!
+        this.println(line.get());
+        numElt--;
     }
 }
--- src/main/java/edu/smith/cs/csc262/coopsh/apps/Tail.java
+++ src/main/java/edu/smith/cs/csc262/coopsh/apps/Tail.java
@@ -1,18 +1,49 @@
 package edu.smith.cs.csc262.coopsh.apps;
 
+import edu.smith.cs.csc262.coopsh.InputLine;
 import edu.smith.cs.csc262.coopsh.ShellEnvironment;
 import edu.smith.cs.csc262.coopsh.Task;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by mltaskova on 2/10/19.
  */
 public class Tail extends Task {
+
+    int numElt;
+    Queue<String> elts = new LinkedList<>();
+
     public Tail(ShellEnvironment env, String[] args) {
         super(env, args);
+        numElt = Integer.parseInt(args[0]);
     }
 
     @Override
     protected void update() {
+        InputLine line = this.input.poll();
+        if (line == null) {
+            // still waiting for more...
+            return;
+        }
 
+        // only output and print when we've seen the whole file!
+        if (line.isEndOfFile()) {
+            for (String l : elts){
+                this.println(l);
+            }
+            this.closeOutput();
+            this.exit(0);
+            return;
+        }
+
+        // Otherwise, increment this count!
+        if (elts.size() == numElt){
+            elts.poll();
+            elts.add(line.get());
+        }
+        else
+            elts.add(line.get());
     }
 }
